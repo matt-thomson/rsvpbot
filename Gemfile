a0c15944dd2e1f44846340962c8752c5acbac1ff
Matt Thomson <mattjohnthomson@gmail.com>
2016-06-11 10:36:17 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source "https://rubygems.org"
+
+gem 'gmail', '~> 0.6.0'
---
+++ rsvpbot.rb
@@ -0,0 +1,24 @@
+require 'csv'
+require 'gmail'
+
+Gmail.connect(:xoauth2, ENV["GMAIL_USERNAME"], ENV["GMAIL_ACCESS_TOKEN"]) do |gmail|
+  CSV.foreach(ARGV[0], headers: true) do |row|
+    gmail.deliver do
+      to row["email"]
+      from "Hayley and Matt <hayley.matt.rsvp@gmail.com>"
+      subject "Wedding RSVP"
+      text_part do
+        body <<~MESSAGE
+          Hi #{row["name"]},
+
+          We haven't heard from you yet about our wedding, and were wondering if you can make it? Please could you let us know by replying to this email, including any dietary requirements you may have?
+
+          Hope you are well!
+
+          Thanks,
+          Hayley and Matt
+        MESSAGE
+      end
+    end
+  end
+end
